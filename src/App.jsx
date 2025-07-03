f7f59f75917306071dbb3ed778340d869f80a6d5
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-03 14:02:16 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
 
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+
+
+
+const App = () => {
+  const [input, setInput] = useState('');
+  const todos = useSelector((state)=> state.todos.todos)
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
 
 
 
