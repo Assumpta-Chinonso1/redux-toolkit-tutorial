e39160fb95ec4ca9b7dfd9fb4eedd36571cfcf89
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-03 13:48:15 +0100

    Thursday commit

--- src/State/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from '../Components/TodoSlice'
-
-export const store = configureStore ({
-    reducer: {
-        todos: todoReducer,
-    }
-})
