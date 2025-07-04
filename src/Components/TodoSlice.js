54311a540bd4a4d77705f469b5a18a264b0a124b
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-04 10:05:11 +0100

    new commit

---
+++ src/Components/TodoSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [],
+}
+
+const todoSlice = createSlice({
+    name: 'todos',
+    initialState,
+    reducers:{
+        addTodos: (state, action) => {
+            state.todos.push({text: action.payload, completed: false})
+        },
+        deleteTodos: (state, action) => {
+            state.todos.splice(action.payload, 1)
+        },
+        toggledTodos: (state, action) => {
+            const todo = state.todos[action.payload]
+            if(todo){
+                todo.completed = ! todo.completed
+            }
+        }
+    }
+})
+
+export const { addTodos, deleteTodos, toggledTodos} = todoSlice.actions;
+export default todoSlice.reducer
